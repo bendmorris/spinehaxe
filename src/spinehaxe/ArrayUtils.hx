44cc98c7ebe27d5f065faa4d45194d111ff0502b
bendmorris <ben@bendmorris.com>
2014-03-18 13:53:47 -0400

    ArrayUtils updates - alloc functions are unnecessary, and indexOf is now an Array method as of Haxe 3.1

--- src/spinehaxe/ArrayUtils.hx
+++ src/spinehaxe/ArrayUtils.hx
@@ -3,32 +3,14 @@ import haxe.ds.Vector;
 
 class ArrayUtils {
     public static function allocFloat(n:Int):Vector<Float> {
-        var a = new Vector<Float>(n);
-        if (n > 0) {
-            a[n - 1] = 0;
-            for (i in 0...(n - 1)) {
-                a[i] = 0;
-            }
-        }
-        return a;
+        return new Vector<Float>(n);
     }
 
     public static function allocString(n:Int):Vector<String> {
-        var a = new Vector<String>(n);
-        if (n > 0) {
-            a[n - 1] = null;
-            for (i in 0...(n - 1)) {
-                a[i] = null;
-            }
-        }
-        return a;
+        return new Vector<String>(n);
     }
 
     public static function indexOf<T>(arr:Array<T>, v:T):Int {
-        #if (flash || js)
-            return untyped arr.indexOf(v);
-        #else
-            return std.Lambda.indexOf(arr, v);
-        #end
+        return arr.indexOf(v);
     }
 }
